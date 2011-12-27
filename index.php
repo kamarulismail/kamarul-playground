1f4e8e2a901bbfbe8ed20bda3ccf9e73bbc625df
Kamarul Ariffin Ismail <kamarul.ismail@gmail.com>
2011-12-27 12:26:38 +0800

    Update: add autoloader for eZ Components

--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../YiiFrameworkLatest/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+$yii    = dirname(__FILE__).'/../YiiFrameworkLatest/yii.php';
+$config = dirname(__FILE__).'/protected/config/main.php';
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
+
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 
 require_once($yii);
+
+// eZ Components
+require_once (dirname(__FILE__) . '/protected/components/ezcomponents/Base/src/base.php');
+Yii::registerAutoloader(array('ezcBase', 'autoload'), true);
+
 Yii::createWebApplication($config)->run();
